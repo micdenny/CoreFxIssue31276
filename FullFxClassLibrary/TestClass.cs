9b2a9ccfd9d4bfa6f92f54a97a030fd5045e09c6
Michael Denny <micdenny@gmail.com>
2018-08-07 15:42:16 +0200

    Add project files.

---
+++ FullFxClassLibrary/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullFxClassLibrary
+{
+    public class TestClass
+    {
+        public static Task<int> DoSomethingAsync()
+        {
+            return Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(1000);
+                return 100;
+            }).Unwrap();
+        }
+    }
+}
